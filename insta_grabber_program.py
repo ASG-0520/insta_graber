66ec5714f2cf1444502bc0f3839d8e779e1a3475
ASG-0520 <ASG-0520@protonmail.com>
2022-04-07 20:04:16 +0300

    PyQt5: refactoring design bugfixes

--- insta_grabber_program.py
+++ insta_grabber_program.py
@@ -5,16 +5,23 @@ import instaloader
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMessageBox, QInputDialog
 
+from pathlib import Path
+import webbrowser
+
 
 class InstaGrabber(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.clipboard = QApplication.clipboard()
-
         self.le_go_btn.clicked.connect(lambda: self.menu())
         self.set_dir_btn.clicked.connect(self.browse_folder)
         self.past_btn.clicked.connect(self.paste_func)
+        self.login_btn.clicked.connect(self.login)
+        self.open_dir_btn.clicked.connect(self.open_folder)
+
+        self.label_dir.setText(str(insta_graber.d_path))
+        self.label_dir.setToolTip(str(insta_graber.d_path))
 
     def paste_func(self):
         self.lineEdit.setText(self.clipboard.text())
@@ -22,7 +29,14 @@ class InstaGrabber(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def browse_folder(self):
         directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
         self.label_dir.setText(directory)
-        insta_graber.L.dirname_pattern = directory
+        self.label_dir.setToolTip(directory)
+        insta_graber.L.dirname_pattern = directory + '/{target}'
+        print('browse_folder: ' + directory)
+
+    def open_folder(self):
+        filename = Path(self.label_dir.text())
+        webbrowser.open(filename.absolute().as_uri())
+        print('open_folder::  ' + str(filename))
 
     def menu(self):
         # ================================================================================ #
@@ -136,6 +150,29 @@ class InstaGrabber(QtWidgets.QMainWindow, design.Ui_MainWindow):
             else:
                 self.show_popup(title='Done', text='DONE ', ico=QMessageBox.Warning)
         # ================================================================================ #
+        # download_top_x_posts
+        # ================================================================================ #
+        elif self.rb_download_top_x_posts.isChecked():
+
+            try:
+                insta_graber.update_profile(self.lineEdit.text())
+
+            except instaloader.exceptions.LoginRequiredException:
+                print('Это закрытый пост, нужно ЗАЛОГИНИТЬСЯ')
+                self.show_popup_login_error()
+                self.login()
+
+            except instaloader.exceptions.ConnectionException:
+                print('нет подключения, проверь инет')
+                self.show_popup_connection_error()
+
+            except IndexError:
+                print("не верный адрес")
+                self.show_popup_index_error()
+            else:
+                self.show_popup(title='Done', text='DONE ', ico=QMessageBox.Warning)
+        # ================================================================================ #
+
 
     def show_popup_login_error(self):
         msg = QMessageBox()
--- insta_graber.py
+++ insta_graber.py
@@ -1,6 +1,5 @@
 import instaloader  # https://instaloader.github.io/module/instaloader.html?highlight=caption
-
-# from settings import log, pas
+from pathlib import Path
 
 """
 Instaloader (Main Class)
@@ -17,9 +16,12 @@ L.save_metadata = False  # json file
 L.post_metadata_txt_pattern = ''  # post caption off (подпись поста)
 L.download_geotags = False
 L.download_comments = False
-L.dirname_pattern = '/Users/aleksandrg./Downloads/Instaloader_downloads'
 L.download_pictures = True
 L.download_videos = True
+
+d_path = Path.home() / 'Downloads' / 'Instaloader_downloads'
+d_path.mkdir(parents=True, exist_ok=True)
+L.dirname_pattern = str(d_path) + '/{target}'
 
 
 def login(log, pas):
@@ -59,18 +61,16 @@ def update_profile(url_of_the_profile):
     L.download_profile(profile_name=profile_name, fast_update=True)
 
 
-def download_top_x_posts():
-    """ It is easy to download the few most-liked pictures of a user in percentage of posts. """
-    from itertools import islice
-    from math import ceil
+def download_top_x_posts(url_of_the_profile):
+    """ It is easy to download the few most-liked posts. """
+    t = 0
+    profile_name = url_of_the_profile.split('/')[3]
+    profile = instaloader.Profile.from_username(L.context, profile_name)
+    posts_sorted_by_likes = sorted(profile.get_posts(), key=lambda p: p.likes, reverse=True)
 
-    profile_to_download_from = input("'Enter the Username profile: ")
-    print("Searching and downloading, plz wait...")
-    x_percentage = 0.1  # percentage of posts that should be downloaded
-    profile = instaloader.Profile.from_username(L.context, profile_to_download_from)
-    posts_sorted_by_likes = sorted(profile.get_posts(),
-                                   key=lambda p: p.likes + p.comments,
-                                   reverse=True)
+    x = 5  # top 5
+    while x != t:
+        for post in posts_sorted_by_likes:
+            L.download_post(post, profile_name)
+            t += 1
 
-    for post in islice(posts_sorted_by_likes, ceil(profile.mediacount * x_percentage / 100)):
-        L.download_post(post, profile_to_download_from)
